a088e98f55f703d44268531d55ce0f08e8a5505c
WarriorFight2Death <22242@DESKTOP-0SHGUOU>
2021-07-02 01:00:59 +0200

    BubbleSort

--- Sorting/Bubble_Sort/Source.cpp
+++ Sorting/Bubble_Sort/Source.cpp
@@ -2,9 +2,31 @@
 #include <cstdlib>
 using namespace std;
 
+void bubbleSort(int arr[], int len) {
 
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = 0; j < len - 1 - i; j++) {
+			int temp;
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
 
 int main() {
+
+	int arr[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+
+	bubbleSort(arr, len);
+
+	for (int i = 0; i < len; i++) {
+		cout << arr[i] << endl;
+	}
+
 
 	return 0;
 }
